4c6e4c78fae5b23543089c93aa483e9a57dd3b61
Cerx <vojtech.duchacek@gmail.com>
2017-08-27 15:34:02 +0200

    Turns out then still fires when there's catch before that, so I've swapped em out.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ class Player extends Model {
 db.register(Server)
 db.register(Player)
 
-const servers = ['my.elkia.life', 'penguin.elkia.cz']
+const servers = ['play.minesplit.com']
 
 async function checkServer (host) {
   let now = new Date()
@@ -28,15 +28,11 @@ async function checkServer (host) {
     type: 'minecraft',
     host: host
   }
-  await Gamedig.query(queryOptions)
-  // if there's an error, set the server status to offline
-      .catch(() => {
-        // query failed, server is offline
-        server.set('status', false)
-      })
+  Gamedig.query(queryOptions)
       // process the data
       .then(queryResponse => {
         // Update server data
+        console.log(queryResponse)
         server.set('version', queryResponse.raw.version)
         server.set('status', true)
 
@@ -46,22 +42,27 @@ async function checkServer (host) {
         queryResponse.players.map(ign => ign.name).forEach(async ign => {
           let player = await Player.findOne({ign})
           player = player === null ? new Player({ign, 'joined': now}) : player // if there's no database entry, create a new one
+          console.log(ign)
 
-          player.get('lastseen').forEach(place => {
-            console.log(place)
-          })
           // I've tried the .increment() method, did't really work, so this is easier I guess
+          //                                if there's no data     set it to 1    othervise add one to the curent value
           player.set(activehour, player.get(activehour) === undefined ? 1 : player.get(activehour) + 1)
           await player.save()
         })
       })
 
+      // if there's an error, set the server status to offline
+      .catch((err) => {
+        // query failed, server is offline
+        console.log(err)
+        server.set('status', false)
+      })
   await server.save()
 }
 
 db.connect().then(() => {
   // run trough the list of servers and log stuff
   servers.forEach((server) => {
-    checkServer(server).then(console.log(`Updated data for '${server}'`))
+    checkServer(server).then(console.log(`Updating data for '${server}'`))
   })
 }).catch(err => console.log(err))
