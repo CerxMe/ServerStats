ebe72fca21eb08501caf917950f94647293e9728
Cerx <vojtech.duchacek@gmail.com>
2017-09-10 17:52:48 +0200

    Implementing session tracking

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Gamedig = require('gamedig')
 
 const dateFormat = require('dateformat')
 
+// Database stuff
 const {Database, Model} = require('mongorito')
 const db = new Database('localhost/ServerStats')
 
@@ -14,7 +15,8 @@ class Player extends Model {
 db.register(Server)
 db.register(Player)
 
-const servers = ['play.minesplit.com']
+// Array of IP's to querry
+const servers = ['my.elkia.life']
 
 async function checkServer (host) {
   let now = new Date()
@@ -28,41 +30,75 @@ async function checkServer (host) {
     type: 'minecraft',
     host: host
   }
-  Gamedig.query(queryOptions)
+
+  await Gamedig.query(queryOptions)
       // process the data
       .then(queryResponse => {
         // Update server data
-        console.log(queryResponse)
         server.set('version', queryResponse.raw.version)
         server.set('status', true)
 
         // Update players
         let activehour = `activity.${dateFormat(now, 'dddd')}.${now.getHours()}`
-
         queryResponse.players.map(ign => ign.name).forEach(async ign => {
           let player = await Player.findOne({ign})
-          player = player === null ? new Player({ign, 'joined': now}) : player // if there's no database entry, create a new one
-          console.log(ign)
+          console.log(`'${host}' : Updating player '${ign}'`)
 
+          // New player entry if theres not one already.
+          player = player === null ? new Player({ign, 'joined': now}) : player
+
+          // Lastseen
+          player.set('lastseen', now)
+
+          // Activity
           // I've tried the .increment() method, did't really work, so this is easier I guess
           //                                if there's no data     set it to 1    othervise add one to the curent value
           player.set(activehour, player.get(activehour) === undefined ? 1 : player.get(activehour) + 1)
+
+          // Sessions
+          if (player.get('sessions') === undefined) {
+            // first session
+            player.set('sessions', [{'active': true, host, 'start': now}])
+          }
           await player.save()
         })
       })
 
-      // if there's an error, set the server status to offline
+      // if there's an error querrying, set the server status to offline
       .catch((err) => {
-        // query failed, server is offline
-        console.log(err)
+        console.log(`'${host}' : ${err}`)
         server.set('status', false)
       })
   await server.save()
 }
 
+// Terminate sessions
+async function sessionTerminator () {
+  // get all players
+  let players = await Player.find()
+
+  // check them one by one
+  await players.forEach(async player => {
+    player.set('sessions',
+        await player.get('sessions').map(session => {
+          // if player has an active session
+          if (session.active) {
+            // but it has been more than 5 minutes (300 seconds) since he's been lastseen, terminate that session
+            if (new Date() - player.get('lastseen') > 300) {
+              session.active = false
+              session.end = player.get('lastseen')
+              console.log(`Terminated session for ${player.get('ign')}`)
+            }
+          }
+        })
+    )
+    await player.save()
+  })
+}
+
 db.connect().then(() => {
   // run trough the list of servers and log stuff
   servers.forEach((server) => {
-    checkServer(server).then(console.log(`Updating data for '${server}'`))
+    checkServer(server).then(sessionTerminator())
   })
 }).catch(err => console.log(err))
